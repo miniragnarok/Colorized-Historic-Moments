8c27a3b63ffadf61570e431c9aa4d650a628e9a3
James Griffin <jamesgriffinprogrammer@outlook.com>
2019-03-07 18:42:28 -0500

    Initial check-in

---
+++ ColorizedHistoricMoments/SQL/UpdateTextures.sql
@@ -0,0 +1,6 @@
+-- Author: minir
+-- DateCreated: 3/6/2019 7:02:21 PM
+--------------------------------------------------------------
+
+UPDATE MomentIllustrations 
+SET Texture = 'CHM_Moment_FirstSeaUnitEarly.dds'
